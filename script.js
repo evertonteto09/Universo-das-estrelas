0cb667867d1b622dc425435b74ca631f243ff219
evertonteto09 <everton.teto.zica.deus@gmail.com>
2026-07-19 19:17:41 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const botao = document.getElementById("explorar");
+
+botao.addEventListener("click", function () {
+
+    document.getElementById("convite").scrollIntoView({
+        behavior: "smooth"
+    });
+
+});
