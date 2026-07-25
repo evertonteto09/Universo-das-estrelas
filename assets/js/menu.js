cf71362fd874eec01c8a63f347adadf4feaeed12
evertonteto09 <everton.teto.zica.deus@gmail.com>
2026-07-25 17:18:13 -0300

    Add files via upload

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -2,6 +2,28 @@ const botaoMenu = document.querySelector(".menu-toggle");
 
 const menu = document.querySelector(".menu-lateral");
 
+const nav = document.querySelector("nav");
+
+const header = document.querySelector("header");
+
+function atualizarMenu(){
+
+    if(window.innerWidth <= 768){
+
+        menu.prepend(nav);
+
+    }else{
+
+        header.insertBefore(nav, menu);
+
+    }
+
+}
+
+window.addEventListener("resize", atualizarMenu);
+
+atualizarMenu();
+
 botaoMenu.addEventListener("click", function(){
 
     menu.classList.toggle("aberto");
